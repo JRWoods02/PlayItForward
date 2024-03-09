796f84ef9abcb93ecd0114c44d1b65a725b33737
Jesse Woods <jesserylanwoods@gmail.com>
2024-03-09 10:41:01 -0500

    setup initial page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PlayItForward
 HackHer 2024 Project on "social good"
+
+A platform that connects sports teams with local social causes, enabling teams to support community projects through volunteering, fundraising matches, or awareness campaigns.
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask import render_template
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return render_template('index.html', message='Hello, World!')
 
 if __name__ == '__main__':
     app.run(debug=True)
